7123bc1391c4c1933f721a991fc466a20b69c569
mechernyshev <m.tchernyshev@gmail.com>
2025-02-20 15:03:55 +0200

    Update version

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -11,6 +11,8 @@ export const errorHandler = (
         res.status(err.statusCode).send({ errors: err.serializeErrors()});
     }
 
+    console.error(err);
+
     res.status(400).send({
         errors: [{ message: 'Something went wrong' }],
     });
